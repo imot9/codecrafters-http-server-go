a0870fc0837cfded47b8ba1536ec093b7fd74b63
Szamel Tamas <szamel.tamas9@gmail.com>
2025-04-12 09:09:27 +0200

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -1,30 +1,55 @@
 package main
 
 import (
-	"errors"
+	"bufio"
 	"fmt"
 	"io"
 	"net"
-	"net/http"
 	"os"
 )
 
+// Ensures gofmt doesn't remove the "net" and "os" imports above (feel free to remove this!)
 var _ = net.Listen
 var _ = os.Exit
 
 func main() {
-	http.HandleFunc("/", getRoot)
+	// You can use print statements as follows for debugging, they'll be visible when running tests.
+	fmt.Println("Logs from your program will appear here!")
 
-	err := http.ListenAndServe(":4221", nil)
-	if errors.Is(err, http.ErrServerClosed) {
-		fmt.Printf("Server closed\n")
-	} else if err != nil {
-		fmt.Printf("Error starting server: %s\n", err)
+	l, err := net.Listen("tcp", "127.0.0.1:4221")
+	if err != nil {
+		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+
+		go handleConnection(conn)
+	}
+
 }
 
-func getRoot(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("got / request\n")
-	io.WriteString(w, "HTTP/1.1 200 OK\r\n\r\n")
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+
+	reader := bufio.NewReader(conn)
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil || line == "\r\n" {
+			break
+		}
+	}
+
+	response := "HTTP/1.1 200 OK\r\n\r\n"
+	_, err := io.WriteString(conn, response)
+
+	if err != nil {
+		fmt.Println("Error sending response:", err.Error())
+		return
+	}
 }
